b207e049bfdb066ff0c6295ffd06ce32bfba7e19
pronazmul <developernazmul@gmail.com>
2022-04-19 20:59:31 +0600

    Worked With input output

---
+++ char.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+char ch;
+printf("Enter Your Name: ");
+scanf("%c", &ch);
+
+printf("Your Name is : %c", ch);
+return 0;
+}
--- hello.c
+++ hello.c
@@ -2,6 +2,34 @@
 
 int main()
 {
-    printf("Welcome Programmer Nazmul Huda");
+    // Integer Value
+    int a=20, b=40, sum;
+    sum = a+b;
+    printf("%d + %d = %d \n", a, b, sum);
+
+    // Double / Float Value inside integer
+    int x = 2.34, y=4, z;
+    z = x + y;
+    printf("%d + %d = %d \n",x, y, z); //Output will be 2 + 4 = 6;
+
+
+    // Double / Float Value
+    double d = 2.34, f;
+    int e=4;
+    f = d + e;
+    printf("%lf + %d = %lf \n",d, e, f);
+
+
+    // Get value from user
+    int m, n, o;
+    //scanf("%d", &m);
+    //scanf("%d", &n);
+
+    //Multiple Togather
+    scanf("%d %d", &m, &n);
+
+    o = m + n;
+    printf("Calculation is : %d" , o);
+
     return 0;
 }
